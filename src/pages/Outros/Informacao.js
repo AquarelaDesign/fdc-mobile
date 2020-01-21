1ec6ba19009729da5a27f71ae4b79cabaf59655e
Jose Procyon <aquarela.design@gmail.com>
2020-01-21 18:02:32 -0300

    Inclusão de outros

---
+++ src/pages/Outros/Informacao.js
@@ -0,0 +1,113 @@
+import React from 'react'
+
+import {
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ImageBackground,
+} from 'react-native'
+
+import GlobalStyles, { _url } from '../../GlobalStyles'
+
+import logo from '../../assets/SimplesDiretObjetivo-branco-sombra.png'
+import bg from '../../assets/fundo-app.png'
+
+const { width, height } = Dimensions.get('window')
+
+export default function Informacao({ navigation }) {
+  
+  const pressLink = () => {
+    navigation.navigate('Browser',{ uri: _url })
+  }
+
+  return (
+    <SafeAreaView style={GlobalStyles.container}>
+      <ImageBackground
+        style={GlobalStyles.background}
+        source={bg}
+      >
+        <Image style={styles.logo} source={logo} />
+
+        <View style={GlobalStyles.boxSpace}>
+        </View>
+        
+        <View style={GlobalStyles.boxContainer}>
+          <Text style={styles.msgTitle}>
+            Simples, direto e objetivo.
+          </Text>
+          <View style={GlobalStyles.boxSpace}>
+          </View>
+
+          <Text style={styles.msgText}>
+            Entre em contato com sua oficina e 
+            solicite o registro dos serviços realizados
+            no seu veículo é rápido, simples e sem custos.
+          </Text>
+          <View style={GlobalStyles.boxSpace}>
+          </View>
+
+          <Text 
+            style={styles.link} 
+            onPress={() => pressLink()}>
+            {` ${_url} `}
+          </Text>
+          <View style={GlobalStyles.boxSpace}>
+          </View>
+
+          <Text style={styles.msgText}>
+            Copyright by Procyon Informática
+          </Text>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 100,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 50,
+  },
+
+  msgTitle: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#FFFFFF',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    // marginTop: height - (height / 2) - 40, 
+    padding: 10,
+    width: width - 10, 
+  },
+
+  msgText: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    // marginTop: height - (height / 2) - 40, 
+    padding: 10,
+    width: width - 10, 
+  },
+
+  link: {
+    color: '#FFFACD', 
+    fontWeight: 'bold',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    padding: 10,
+    width: width - 10, 
+  },
+
+})
---
+++ src/pages/Outros/Outros.js
@@ -0,0 +1,98 @@
+import React from 'react'
+
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity 
+} from 'react-native'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import GlobalStyles from '../../GlobalStyles'
+
+import logo from '../../assets/SimplesDiretObjetivo-branco-sombra.png'
+import bg from '../../assets/fundo-app.png'
+
+import ExcluiVeiculos from '../../assets/ExcluiVeiculos.png'
+import Mensagens from '../../assets/Mensagens.png'
+import ConsultaPJ from '../../assets/ConsultaPJ.png'
+import FAQ from '../../assets/FAQ.png'
+import Informacao from '../../assets/Informacao.png'
+
+
+export default function Outros({ navigation }) {
+  const onPress = (tipo) => {
+    switch (tipo) {
+      // case 'REC':  navigation.navigate('Recebimentos'); break
+      case 'INF': navigation.navigate('Informacao'); break
+      default: Alert.alert(`Clicado em ${tipo}`); break
+    }
+  }
+  
+  // style={[GlobalStyles.AndroidSafeArea, styles.container]}
+  return (
+    <SafeAreaView style={GlobalStyles.container}>
+      <ImageBackground
+        style={GlobalStyles.background}
+        source={bg}
+      >
+        <Image style={styles.logo} source={logo} />
+
+        <View style={GlobalStyles.boxSpace}>
+        </View>
+        
+        <View style={GlobalStyles.boxContainer}>
+          <TouchableOpacity activeOpacity = { .5 }  onPress={() => onPress('EXC')}>
+            <View style={GlobalStyles.box}>
+              {/* <Icon name="tags" size={60} color="#007189" /> */}
+              <Image style={GlobalStyles.boxIcone} source={ExcluiVeiculos} />
+              <Text style={GlobalStyles.boxText}>Exclui Veículos</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity = { .5 }  onPress={() => onPress('MEN')}>
+            <View style={GlobalStyles.box}>
+              <Image style={GlobalStyles.boxIcone} source={Mensagens} />
+              <Text style={GlobalStyles.boxText}>Mensagens</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity = { .5 }  onPress={() => onPress('CON')}>
+            <View style={GlobalStyles.box}>
+              <Image style={GlobalStyles.boxIcone} source={ConsultaPJ} />
+              <Text style={GlobalStyles.boxText}>Consulta PJ</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity = { .5 }  onPress={() => onPress('FAQ')}>
+            <View style={GlobalStyles.box}>
+              <Image style={GlobalStyles.boxIcone} source={FAQ} />
+              <Text style={GlobalStyles.boxText}>FAQ</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity = { .5 }  onPress={() => onPress('INF')}>
+            <View style={GlobalStyles.box}>
+              <Image style={GlobalStyles.boxIcone} source={Informacao} />
+              <Text style={GlobalStyles.boxText}>Informação</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity = { .5 }>
+            <View style={GlobalStyles.box}></View>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 100,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 50,
+  },
+
+})
