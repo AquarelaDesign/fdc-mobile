52062bf220fea4a032a70031eeef9c94d686556a
Jose Procyon <aquarela.design@gmail.com>
2020-01-24 18:21:30 -0300

    Ocultado mensagem de depuração YellowBox

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ YellowBox.ignoreWarnings([
   'Unrecognized WebSocket',
   'Error: timeout of',
   "Warning: Can't perform a",
+  "Possible Unhandled Promise Rejection",
 ])
 
 export { default } from './src/routes'
