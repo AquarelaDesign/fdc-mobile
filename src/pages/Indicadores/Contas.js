cd79f25c06b8c312508f978af7879df6f423a046
Jose Augusto Freire <aquarela.design@gmail.com>
2020-02-20 09:29:05 -0300

    Inclusao dos parametros de periodo

--- src/pages/Indicadores/Contas.js
+++ src/pages/Indicadores/Contas.js
@@ -2,24 +2,36 @@ import React, { useState, useEffect } from 'react'
 
 import {
   AsyncStorage,
+  Button,
   Dimensions,
+  Image,
   SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
   ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from 'react-native'
 
-import { ListItem } from 'react-native-elements'
-import Lottie from 'lottie-react-native'
-import Icon from 'react-native-vector-icons/FontAwesome'
-
 import { LinearGradient } from '../../components/LinearGradient'
-import { dataInicial, dataFinal } from '../../globais'
-import Api from '../../services/oapi'
-import GlobalStyles from '../../GlobalStyles'
+
+import Lottie from 'lottie-react-native'
+import GlobalStyles, { modalStyle } from '../../GlobalStyles'
+
 import loading from '../../assets/json/car-scan.json'
 
+import Api from '../../services/oapi'
+
+import { ListItem, Overlay, Divider  } from 'react-native-elements'
+import NumberFormat from 'react-number-format'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+import btnLogo from '../../assets/filter.png'
+
+import { dataInicial, dataFinal } from '../../globais'
+import DatePicker from 'react-native-datepicker'
+
+const { width } = Dimensions.get('window')
 const querystring = require('querystring')
 
 const Contas = () => {
@@ -27,103 +39,113 @@ const Contas = () => {
   const [email, setEmail] = useState('')
   const [cons, setCons] = useState([])
   const [resumo, setResumo] = useState([])
+  const [modView, setModView] = useState(false)
+  const [dtInicio, setDtInicio] = useState(dataInicial)
+  const [dtFinal, setDtFinal] = useState(dataFinal)
 
   useEffect(() => {
     setIsLoading(true)
 
-    async function montaLista(dados) {
-      let ind = []
-      
-      ind.push({
-        icon: "bank",
-        title: "Contas a Pagar",
-        subtitle: `${retValor(dados.totpag, 'currency')}`,
-        linearGradientColors: ['#061700', '#56ab2f'],
-        valor: `${retValor(dados.totpag, 'currency')}`
-      })
-
-      ind.push({
-        icon: "money",
-        title: "Contas a Receber",
-        subtitle: `${retValor(dados.totrec, 'currency')}`,
-        linearGradientColors: ['#2193b0', '#134E5E'],
-        valor: `${retValor(dados.totrec, 'currency')}`
-      })
-
-      const icone = dados.totrec > dados.totpag ? 'thumbs-up' : 'thumbs-down'
-      const saldo = dados.totrec - dados.totpag
-      const gdcor = dados.totrec > dados.totpag ? ['#4CAF50', '#8BC34A'] : ['#F44336', '#E91E63']
-
-      ind.push({
-        icon: icone,
-        title: "Saldo",
-        subtitle: `${retValor(saldo, 'currency')}`,
-        linearGradientColors: gdcor,
-        valor: `${retValor(saldo, 'currency')}`
-      })
-
-      setCons(ind)
-      setIsLoading(false)
-    }
-
     AsyncStorage.getItem('email').then(Email => {
       setEmail(Email)
-      if (email !== '') {
-        async function buscaContas() {
-          try {
-            await Api.post('', querystring.stringify({
-              pservico: 'wfcfin',
-              pmetodo: 'ListaResumo',
-              pcodprg: 'TFCFIN',
-              pemail: email,
-              pdtini: dataInicial,
-              pdtfim: dataFinal,
-              ptipcon: 'A',
-            })).then(response => {
-              if (response.status === 200) {
-                if (response.data.ProDataSet !== undefined) {
-                  const { ttresumo } = response.data.ProDataSet
-                  setResumo(ttresumo)
-                  // console.log('ttresumo', ttresumo)
-
-                  let totpag = 0
-                  let totrec = 0
-                  let qtpag = 0
-                  let qtrec = 0
-                  
-                  ttresumo.forEach((value, key) => {
-                    totpag += value.totpag
-                    totrec += value.totrec
-                    qtpag += value.qtpag
-                    qtrec += value.qtrec
-                  })
-    
-                  const totais = { 
-                    totpag,
-                    totrec,
-                    qtpag,
-                    qtrec
-                  }
-                  montaLista(totais)
-                }
-              } 
-              setIsLoading(false)
-            })
-          } catch (error) {
-            const { response } = error
-            if (response !== undefined) {
-              setIsLoading(false)
-            } else {
-              setIsLoading(false)
-            }
-          }
-        }
-        buscaContas()
-      }
+      buscaDados()
     })
   }, [email])
 
-  function retValor(valor, tipo) {
+  const buscaDados = async () => {
+    setModView(false)
+    setIsLoading(true)
+
+    if (email !== '') {
+      async function buscaContas() {
+        try {
+          await Api.post('', querystring.stringify({
+            pservico: 'wfcfin',
+            pmetodo: 'ListaResumo',
+            pcodprg: 'TFCFIN',
+            pemail: email,
+            pdtini: dtInicio,
+            pdtfim: dtFinal,
+            ptipcon: 'A',
+          })).then(response => {
+            if (response.status === 200) {
+              if (response.data.ProDataSet !== undefined) {
+                const { ttresumo } = response.data.ProDataSet
+                setResumo(ttresumo)
+                // console.log('ttresumo', ttresumo)
+
+                let totpag = 0
+                let totrec = 0
+                let qtpag = 0
+                let qtrec = 0
+                
+                ttresumo.forEach((value, key) => {
+                  totpag += value.totpag
+                  totrec += value.totrec
+                  qtpag += value.qtpag
+                  qtrec += value.qtrec
+                })
+  
+                const totais = { 
+                  totpag,
+                  totrec,
+                  qtpag,
+                  qtrec
+                }
+                montaLista(totais)
+              }
+            } 
+            setIsLoading(false)
+          })
+        } catch (error) {
+          const { response } = error
+          if (response !== undefined) {
+            setIsLoading(false)
+          } else {
+            setIsLoading(false)
+          }
+        }
+      }
+      buscaContas()
+    }
+  }
+
+  const montaLista = async (dados) => {
+    let ind = []
+    
+    ind.push({
+      icon: "bank",
+      title: "Contas a Pagar",
+      subtitle: `${retValor(dados.totpag, 'currency')}`,
+      linearGradientColors: ['#061700', '#8BC34A'],
+      valor: `${retValor(dados.totpag, 'currency')}`
+    })
+
+    ind.push({
+      icon: "money",
+      title: "Contas a Receber",
+      subtitle: `${retValor(dados.totrec, 'currency')}`,
+      linearGradientColors: ['#061700', '#E91E63'],
+      valor: `${retValor(dados.totrec, 'currency')}`
+    })
+
+    const icone = dados.totrec > dados.totpag ? 'thumbs-up' : 'thumbs-down'
+    const saldo = dados.totrec - dados.totpag
+    const gdcor = dados.totrec > dados.totpag ? ['#4CAF50', '#8BC34A'] : ['#F44336', '#E91E63']
+
+    ind.push({
+      icon: icone,
+      title: "Saldo",
+      subtitle: `${retValor(saldo, 'currency')}`,
+      linearGradientColors: gdcor,
+      valor: `${retValor(saldo, 'currency')}`
+    })
+
+    setCons(ind)
+    setIsLoading(false)
+  }
+
+  const retValor = (valor, tipo) => {
     let xVal = 0
 
     if (valor !== undefined || valor !== null) {
@@ -138,7 +160,7 @@ const Contas = () => {
     }
   }
 
-  function Loading() {
+  const Loading = () => {
     return (
       <Lottie source={loading} autoPlay loop />
     )
@@ -146,10 +168,109 @@ const Contas = () => {
 
   return (
     <SafeAreaView style={[GlobalStyles.container, {paddingTop: 15,}]}>
+      <Overlay
+        isVisible={modView}
+        supportedOrientations={['portrait', 'landscape']}
+        windowBackgroundColor="rgba(0, 0, 0, .7)"
+        overlayBackgroundColor="transparent"
+        width="80%"
+        height="40%"
+        overlayStyle={{
+          backgroundColor: 'white',
+          borderRadius: 15,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.8,
+          shadowRadius: 5,
+        }}
+        onBackdropPress={() => {
+          setModView(false)
+        }}>
+
+        <View style={modalStyle.modalContainer}>
+          <View style={modalStyle.innerContainer}>
+            
+            <View style={modalStyle.form}>
+              <Text style={modalStyle.label}>Data Inicial</Text>
+              <DatePicker
+                style={{
+                  width: 200,
+                  marginBottom: 10
+                }}
+                date={dtInicio}
+                mode='date'
+                placeholder="Data Inicial"
+                format="DD/MM/YYYY"
+                confirmBtnText="Confirmar"
+                cancelBtnText="Cancelar"
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                  },
+                  dateInput: {
+                    marginLeft: 36, 
+                    backgroundColor: '#ccc'
+                  }
+                }}
+                onDateChange={(date) => {setDtInicio(date)}}
+              />
+
+              <Text style={modalStyle.label}>Data Final</Text>
+              <DatePicker
+                style={{
+                  width: 200,
+                  marginBottom: 10
+                }}
+                date={dtFinal}
+                mode='date'
+                placeholder="Data Final"
+                format="DD/MM/YYYY"
+                confirmBtnText="Confirmar"
+                cancelBtnText="Cancelar"
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                  },
+                  dateInput: {
+                    marginLeft: 36, 
+                    backgroundColor: '#ccc'
+                  }
+                }}
+                onDateChange={(date) => {setDtFinal(date)}}
+              />
+              
+              <Button
+                style={{
+                  marginBottom: 20
+                }}
+                onPress={() => {buscaDados()}}
+                title="Filtrar"
+              >
+              </Button>
+            </View>
+          </View>
+        </View>
+      </Overlay>
+      
       <View style={styles.row}>
-        <Icon name="calculator" size={40} color="#007189" style={{marginLeft: 20, marginTop: 30, marginBottom: 10, }}/>
+        <Icon name="money" size={40} color="#f7ff00" style={{marginLeft: 20, marginTop: 30, marginBottom: 10, }}/>
         <Text style={styles.title}>Contas</Text>
+        <TouchableOpacity activeOpacity = { .5 }  onPress={() => setModView(true)}>
+          <Image style={modalStyle.boxIcone} source={btnLogo} tintColor='#FFFFFF'/>
+        </TouchableOpacity>
       </View>
+
+      <Divider style={{ backgroundColor: 'gray' }} />
+      <View style={styles.row}>
+        <Text style={styles.subtitle}>{`Período de ${dtInicio} até ${dtFinal}`}</Text>
+      </View>
+      <Divider style={{ backgroundColor: 'gray' }} />
 
       <ScrollView>
         <View style={styles.list}>
@@ -162,7 +283,7 @@ const Contas = () => {
                 color: 'blue',
               }}
               title={l.title}
-              titleStyle={{ color: '#f7ff00', fontWeight: 'bold', fontSize: 17, }}
+              titleStyle={{ color: '#f7ff00', fontWeight: 'bold', fontSize: 13, }}
               subtitle={l.subtitle}
               subtitleStyle={{ color: 'white', fontSize: 14,  }}
               linearGradientProps={{
@@ -202,12 +323,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
     color: '#FFF',
-    width: Dimensions.get('window').width - 10,
+    width: width - 115,
     paddingHorizontal: 20,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 30,
     textTransform: "uppercase",
+  },
+
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#ff0',
+    width: width - 10,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5,
   },
 
 })
